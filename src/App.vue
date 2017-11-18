0a3bc3204525f004ea28307e8ae13393ef031364
saki <sakihet@gmail.com>
2017-11-18 12:08:25 +0900

    implement

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+<template lang="pug">
+#app
+  h1 Meishi Maker
+  .grid
+    .grid-cell
+      MeishiCanvas(
+        :leftTopTexts="getLeftTopTexts"
+        :backgroundColor="getBgColor"
+      )
+    .grid-cell.card-params
+      input(type="text" v-model="name")
+      br
+      input(type="text" v-model="job")
+      br
+      input(type="text" v-model="url")
+      br
+      input(type="text" v-model="mailaddress")
+      br
+      input(type="text" v-model="organization")
+      br
+      input(type="text" v-model="backgroundColor")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MeishiCanvas from './components/atoms/MeishiCanvas'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MeishiCanvas
+  },
+  data () {
+    return {
+      name: 'hello, world',
+      job: 'job',
+      url: 'example.com',
+      mailaddress: 'test@example.com',
+      organization: 'organization',
+      backgroundColor: '#eeeeee'
+    }
+  },
+  computed: {
+    getLeftTopTexts: function () {
+      return [this.name, this.job, this.url, this.mailaddress, this.organization]
+    },
+    getBgColor: function () {
+      return this.backgroundColor
+    }
+  },
+  methods: {
   }
 }
 </script>
@@ -23,6 +59,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.grid {
+  display: flex;
+  flex-direction: rows;
+}
+.grid-cell {
+  flex: 1;
+}
+.card-params {
+  text-align: left;
 }
 </style>
---
+++ src/components/atoms/MeishiCanvas.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+div
+  canvas#cv
+  // TODO: fix bug
+  p(hidden) {{ leftTopTexts }}
+  p(hidden) {{ backgroundColor }}
+</template>
+
+<script>
+export default {
+  name: 'MeishiCanvas',
+  props: {
+    'leftTopTexts': Array,
+    'backgroundColor': String
+  },
+  mounted () {
+    this.drawCanvas()
+  },
+  updated () {
+    this.drawCanvas()
+  },
+  methods: {
+    drawCanvas: function () {
+      console.log('draw')
+      let cv = document.getElementById('cv')
+      let ctx = cv.getContext('2d')
+      ctx.clearRect(0, 0, 345, 209)
+      ctx.fillStyle = this.backgroundColor
+      ctx.fillRect(0, 0, 345, 209)
+      ctx.fillStyle = '#000000'
+      ctx.font = '12px monospace'
+      let texts = this.leftTopTexts
+      for (let i = 0; i < texts.length; i++) {
+        ctx.fillText(texts[i], 20, 20 + i * 12)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+canvas {
+  border: 1px solid;
+  width: 345px;
+  height: 209px;
+}
+</style>
